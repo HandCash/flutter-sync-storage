036197bf2d0b2514791a08b6f6e381b56532b6f4
Luis Palomo <l.palomo@handcash.io>
2020-01-09 15:06:40 +0200

    Fix build

--- android/src/main/java/io/handcash/flutter_sync_storage/SyncBackupHelper.java
+++ android/src/main/java/io/handcash/flutter_sync_storage/SyncBackupHelper.java
@@ -4,11 +4,7 @@ import android.app.backup.BackupAgentHelper;
 import android.app.backup.SharedPreferencesBackupHelper;
 import android.util.Log;
 
-import io.flutter.embedding.engine.systemchannels.PlatformChannel;
-
 public class SyncBackupHelper extends BackupAgentHelper {
-
-    PlatformChannel _channel = 
 
     // The name of the SharedPreferences file
     static final String PREFS = "io.handcash.flutter_sync_storage.android_backup";
